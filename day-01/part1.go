0a8f5fae77371d0833bed1169ca7379069e70aee
Josh Borrow <josh@joshborrow.com>
2023-12-02 08:01:31 -0500

    Code formatting

--- day-01/part1.go
+++ day-01/part1.go
@@ -11,47 +11,47 @@ import "bufio"
 var just_numbers_regex = regexp.MustCompile(`[0-9]`)
 
 func single_line(str string) int {
-    sub_string := just_numbers_regex.FindAllString(str, -1)
-    n_strings := len(sub_string)
+	sub_string := just_numbers_regex.FindAllString(str, -1)
+	n_strings := len(sub_string)
 
-    // Deal with the fact we may have more than one character
-    first := string(sub_string[0])
-    final := first
+	// Deal with the fact we may have more than one character
+	first := string(sub_string[0])
+	final := first
 
-    if (n_strings > 1) {
-        final = string(sub_string[n_strings - 1])
-    } 
+	if n_strings > 1 {
+		final = string(sub_string[n_strings-1])
+	}
 
-    first_and_last := strings.TrimSpace(
-        first + final,
-    )
+	first_and_last := strings.TrimSpace(
+		first + final,
+	)
 
-    real_int, err := strconv.Atoi(first_and_last)
+	real_int, err := strconv.Atoi(first_and_last)
 
-    if (err != nil) {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return real_int
+	return real_int
 }
 
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    total := 0;
+	total := 0
 
-    for scanner.Scan() {
-        text := scanner.Text()
+	for scanner.Scan() {
+		text := scanner.Text()
 
-        single := single_line(text)
-        fmt.Println("Output: ", single, " Base: ", text)
+		single := single_line(text)
+		fmt.Println("Output: ", single, " Base: ", text)
 
-        total += single
-    }
+		total += single
+	}
 
-    if scanner.Err() != nil {
-        log.Fatal("No input provided.")
-    }
+	if scanner.Err() != nil {
+		log.Fatal("No input provided.")
+	}
 
-    fmt.Println("Total: ", total)
+	fmt.Println("Total: ", total)
 }
--- day-01/part2.go
+++ day-01/part2.go
@@ -7,80 +7,80 @@ import "regexp"
 import "bufio"
 
 var string_map = map[string]int{
-    "0": 0,
-    "1": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+	"0":     0,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
 }
 
 func set_regex() *regexp.Regexp {
-    regex := ""
+	regex := ""
 
-    for k := range string_map {
-        regex += string(k + "|")
-    }
+	for k := range string_map {
+		regex += string(k + "|")
+	}
 
-    return regexp.MustCompile(regex[:len(regex)-1])
+	return regexp.MustCompile(regex[:len(regex)-1])
 }
 
 var just_numbers_regex = set_regex()
 
 func single_line_multimatch(str string) int {
-    // First match is super easy.
-    first_match := string_map[just_numbers_regex.FindString(str)]
-    final_match := -1
+	// First match is super easy.
+	first_match := string_map[just_numbers_regex.FindString(str)]
+	final_match := -1
 
-    for i := len(str); i >= 0; i-- {
-        match := just_numbers_regex.FindString(str[i:])
+	for i := len(str); i >= 0; i-- {
+		match := just_numbers_regex.FindString(str[i:])
 
-        if (match != "") {
-            // First match going backwards!
-            final_match = string_map[match];
-            break;
-        }
-    }
+		if match != "" {
+			// First match going backwards!
+			final_match = string_map[match]
+			break
+		}
+	}
 
-    // Combine back together
-    return 10 * first_match + final_match
+	// Combine back together
+	return 10*first_match + final_match
 }
 
 func main() {
-    DEBUG := false
+	DEBUG := false
 
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    total := 0;
+	total := 0
 
-    for scanner.Scan() {
-        text := scanner.Text()
+	for scanner.Scan() {
+		text := scanner.Text()
 
-        single := single_line_multimatch(text)
+		single := single_line_multimatch(text)
 
-        if (DEBUG) {
-            fmt.Println("Output: ", single, " Base: ", text)
-        }
+		if DEBUG {
+			fmt.Println("Output: ", single, " Base: ", text)
+		}
 
-        total += single
-    }
+		total += single
+	}
 
-    if scanner.Err() != nil {
-        log.Fatal("No input provided.")
-    }
+	if scanner.Err() != nil {
+		log.Fatal("No input provided.")
+	}
 
-    fmt.Println("Total: ", total)
+	fmt.Println("Total: ", total)
 }
