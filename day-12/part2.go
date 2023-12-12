05841bfd7653196fab5ed75a41953fac039bd11a
Josh Borrow <josh@joshborrow.com>
2023-12-12 12:25:12 -0500

    Hmm memoization in go lol

--- day-12/part2.go
+++ day-12/part2.go
@@ -51,164 +51,20 @@ func parse_row(row string) ([]uint8, []uint8) {
 	return status, pattern
 }
 
-func matches_pattern(row []uint8, pattern []uint8) bool {
-	start := 0
+func to_string(row []uint8) string {
+	result := ""
 
-	for i, value := range row {
-		if value != OPERATIONAL {
-			start = i
-			break
-		}
-	}
-
-	current_pattern_index := 0
-	matched_in_pattern := uint8(0)
-	current_pattern_to_match := pattern[current_pattern_index]
-
-	matched := make([]bool, len(pattern))
-
-	for i := start; i < len(row); i++ {
-		if row[i] == BROKEN {
-			if current_pattern_to_match > matched_in_pattern {
-				matched_in_pattern += 1
-
-				if matched_in_pattern == current_pattern_to_match {
-					// We have matched the current pattern
-					matched[current_pattern_index] = true
-				}
-			} else {
-				// We have broken the current pattern
-				return false
-			}
-		} else if row[i] == OPERATIONAL {
-			// Skip to next pattern.
-			if matched_in_pattern == 0 {
-				continue
-			}
-
-			current_pattern_index += 1
-			matched_in_pattern = 0
-
-			if current_pattern_index >= len(pattern) {
-				// We have matched all the patterns
-				// Check if there are any more BROKENs
-				for j := i; j < len(row); j++ {
-					if row[j] == BROKEN {
-						return false
-					}
-				}
-
-				break
-			}
-
-			current_pattern_to_match = pattern[current_pattern_index]
-		}
-	}
-
-	// Did we match all the patterns?
-	for _, match := range matched {
-		if !match {
-			return false
-		}
-	}
-
-	return true
-}
-
-func contains_orphans(row []uint8, pattern []uint8) bool {
-	start := 0
-
-	for i, value := range row {
-		if value != OPERATIONAL {
-			start = i
-			break
-		}
-	}
-
-	current_pattern_index := 0
-	matched_in_pattern := uint8(0)
-	current_pattern_to_match := pattern[current_pattern_index]
-
-	matched := make([]bool, len(pattern))
-
-	for i := start; i < len(row); i++ {
-		if row[i] == UNKNOWN {
-			// It's all over!
-			return false
-		}
-		if row[i] == BROKEN {
-			if current_pattern_to_match > matched_in_pattern {
-				matched_in_pattern += 1
-
-				if matched_in_pattern == current_pattern_to_match {
-					// We have matched the current pattern
-					matched[current_pattern_index] = true
-				}
-			} else {
-				// We have broken the current pattern
-				return true
-			}
-		} else if row[i] == OPERATIONAL {
-			// Skip to next pattern.
-			if matched_in_pattern == 0 {
-				continue
-			}
-
-			current_pattern_index += 1
-			matched_in_pattern = 0
-
-			if current_pattern_index >= len(pattern) {
-				// We have matched all the patterns
-				// Check if there are any more BROKENs
-				for j := i; j < len(row); j++ {
-					if row[j] == BROKEN {
-						return true
-					}
-				}
-
-				break
-			}
-
-			current_pattern_to_match = pattern[current_pattern_index]
-		}
-	}
-
-	// Did we match all the patterns?
-	for _, match := range matched {
-		if !match {
-			return true
-		}
-	}
-
-	return false
-}
-
-func will_ever_match(row []uint8, pattern []uint8) bool {
-	// We can trim branches if there aren't enough combos left
-	total_number_of_hashes := 0
-	for _, value := range pattern {
-		total_number_of_hashes += int(value)
-	}
-
-	// If there are more hashes than there are hashes and unknowns, we can't
-	// possibly match.
-	total := 0
 	for _, value := range row {
-		if value == UNKNOWN || value == BROKEN {
-			total += 1
+		if value == UNKNOWN {
+			result += "?"
+		} else if value == OPERATIONAL {
+			result += "."
+		} else if value == BROKEN {
+			result += "#"
 		}
 	}
 
-	if total < total_number_of_hashes {
-		return false
-	}
-
-	// If we have orphans at the start, or end, we can't match.
-	if contains_orphans(row, pattern) {
-		return false
-	}
-
-	return true
+	return result
 }
 
 func print(row []uint8) {
@@ -225,82 +81,182 @@ func print(row []uint8) {
 	fmt.Println()
 }
 
-func replace_and_continue(
-	row []uint8,
-	index int,
-	pattern []uint8,
-	total_matches *int,
-) {
-	// Base case
-	if index == len(row) {
-		// Can actually check our row for matches
-		if matches_pattern(row, pattern) {
-			*total_matches += 1
-			return
-		} else {
-			return
+func consume_and_continue(status []uint8, pattern []uint8, starting_index int, cache *map[string]int) int {
+	// Now check if we have already computed this.
+	hash := fmt.Sprintf("%v-%v", starting_index, pattern)
+	if value, ok := (*cache)[hash]; ok {
+		// fmt.Println("Found cached value: ", value)
+		return value
+	}
+
+	// Base case; there are no more patterns left to match. We have matched!
+	if len(pattern) == 0 {
+		// fmt.Println("Matched end of pattern!")
+		(*cache)[hash] = 1
+		return 1
+	}
+
+	// Base case: there are no more status left to match. We have failed!
+	if len(status) <= starting_index {
+		// fmt.Println("Failed to match end of status!")
+		(*cache)[hash] = 0
+		return 0
+	}
+
+	// Base case: we have ran out of characters
+	left_over_characters := 0
+	for _, value := range status[starting_index:] {
+		if value == UNKNOWN || value == BROKEN {
+			left_over_characters += 1
 		}
 	}
 
-	if !will_ever_match(row, pattern) {
-		return
+	need_to_match_total := 0
+	for _, value := range pattern {
+		need_to_match_total += int(value)
 	}
 
-	// Recursive case
-	if row[index] == UNKNOWN {
-		// Replace with BROKEN
-		new_arr := make([]uint8, len(row))
-		copy(new_arr, row)
-		new_arr[index] = BROKEN
-
-		replace_and_continue(
-			new_arr,
-			index+1,
-			pattern,
-			total_matches,
-		)
-
-		// Replace with OPERATIONAL
-		copy(new_arr, row)
-		new_arr[index] = OPERATIONAL
-
-		replace_and_continue(
-			new_arr,
-			index+1,
-			pattern,
-			total_matches,
-		)
-	} else {
-		// Just continue
-		replace_and_continue(
-			row,
-			index+1,
-			pattern,
-			total_matches,
-		)
+	if left_over_characters < need_to_match_total {
+		// fmt.Println("Ran out of characters! Have", left_over_characters, "but need", need_to_match_total)
+		// fmt.Println("Status: ", status[starting_index:])
+		(*cache)[hash] = 0
+		return 0
 	}
 
-	return
+	need_to_consume := pattern[0]
+
+	for i := starting_index; i < len(status); i++ {
+		this_status := status[i]
+		// fmt.Println("Considering: ", this_status, "with need_to_consume: ", need_to_consume)
+
+		if need_to_consume == 0 {
+			if this_status == BROKEN {
+				// Uh, I need to consume nothing, but I have to!
+				(*cache)[hash] = 0
+				return 0
+			}
+			// fmt.Println("Have consumed all that I need to consume.")
+			// The next one MUST be an OPERATIONAL or we can't match
+			// Are we at the end of the array?
+			if i+1 == len(status) {
+				// We have matched!
+				// fmt.Println("Matched up to my point but reached end of status!")
+				if len(pattern) == 1 {
+					(*cache)[hash] = 1
+					return 1
+				} else {
+					(*cache)[hash] = 0
+					return 0
+				}
+			}
+
+			if status[i] == OPERATIONAL || status[i] == UNKNOWN {
+				// Try all possible starting points.
+				if len(pattern[1:]) == 0 {
+					for _, value := range status[i+1:] {
+						if value == BROKEN {
+							// We have left overs :(
+							(*cache)[hash] = 0
+							return 0
+						}
+					}
+					// We don't actually need to match anything else.
+					(*cache)[hash] = 1
+					return 1
+				}
+
+				// fmt.Println("Trying all possible starting points. Still need to match: ", pattern[1:], "with status: ", to_string(status[i+1:]))
+
+				total_matches := 0
+				for j := i + 1; j < len(status); j++ {
+					if status[j] == OPERATIONAL {
+						// No point in launching from here. We will just double count!
+						continue
+					}
+					individual_matches := consume_and_continue(
+						status,
+						pattern[1:],
+						j,
+						cache,
+					)
+					// if individual_matches > 0 {
+					// 	// fmt.Println("Found individual matches: ", individual_matches, "with status: ", to_string(status[j:]), "and pattern: ", pattern[1:])
+					// }
+					total_matches += individual_matches
+					if status[j] == BROKEN {
+						// Cannot go past this point! Recursion must handle the rest. I MUST match hashes.
+						break
+					}
+				}
+				// fmt.Println("Found total matches: ", total_matches)
+				(*cache)[hash] = total_matches
+				return total_matches
+			} else {
+				(*cache)[hash] = 0
+				return 0
+			}
+		}
+
+		if this_status == UNKNOWN || this_status == BROKEN {
+			// fmt.Println("Matching unknown or broken at position: ", i, " with need_to_consume: ", need_to_consume)
+			need_to_consume -= 1
+			continue
+		}
+
+		if this_status == OPERATIONAL {
+			// Fall out
+			(*cache)[hash] = 0
+			return 0
+		}
+	}
+
+	if need_to_consume == 0 {
+		if len(pattern) == 1 {
+			// fmt.Println("Matched up to my point!")
+			(*cache)[hash] = 1
+			return 1
+		}
+	}
+
+	(*cache)[hash] = 0
+	return 0
 }
 
-func find_total_matches(status []uint8, pattern []uint8) int {
-	var total_matches *int
-	total_matches = new(int)
-	*total_matches = 0
-	replace_and_continue(status, 0, pattern, total_matches)
-	return *total_matches
+func consume_all(
+	status []uint8,
+	pattern []uint8,
+) int {
+	total_matches := 0
+	cache := make(map[string]int)
+
+	for i := 0; i < len(status); i++ {
+		if status[i] == OPERATIONAL {
+			// No point in launching from here. We will just double count!
+			continue
+		}
+
+		// fmt.Println("Entering consume_and_continue with status: ", status[i:], "and pattern: ", pattern)
+		total_matches += consume_and_continue(
+			status,
+			pattern,
+			i,
+			&cache,
+		)
+		// fmt.Println("Exited consume_and_continue with n total matches", total_matches)
+
+		if status[i] == BROKEN {
+			// Cannot go past this point! Recursion must handle the rest.
+			break
+		}
+	}
+
+	return total_matches
 }
 
 func parse_and_return(row string) int {
 	status, pattern := parse_row(row)
-	matches := find_total_matches(status, pattern)
+	matches := consume_all(status, pattern)
 	return matches
-}
-
-func parse_and_send(row string, c chan int) {
-	status, pattern := parse_row(row)
-	matches := find_total_matches(status, pattern)
-	c <- matches
 }
 
 func main() {
@@ -326,7 +282,7 @@ func main() {
 	for _, row := range all_rows {
 		these_matches := parse_and_return(row)
 		if DEBUG {
-			fmt.Println("Matches: ", these_matches)
+			fmt.Println("Total matches: ", these_matches)
 		}
 		all_matches += these_matches
 	}
